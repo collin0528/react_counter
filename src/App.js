e7090c6e48b6b35f946f78418162eabbb4a9ced3
collin0528 <collinsndukwe28@gmail.com>
2022-04-29 15:10:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import Increasedecreas from './Increasedecreas';
+
+class App extends React.Component{
+
+    constructor(){
+      super()
+      this.username = "James"
+      
+
+    }
+
+
+  
+
+
+    render(){
+
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Greeting personname={this.username} age='12' height='5.2' email='james@email.com'>Hello everyone</Greeting>
+          
+            <Button/>
+          </header>
+        </div>
+      );
+
+    }
+
+}
+
+export default App
